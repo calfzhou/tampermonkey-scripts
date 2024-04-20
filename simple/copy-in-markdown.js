cc26ebaad78cdc4f1cf56dd6f77fe02f49dc7412
Ji ZHOU <calf.zhou@gmail.com>
2024-04-20 22:15:01 +0800

    feat(copy-in-markdown): v1.0.1 - add a variant for hexo

--- simple/copy-in-markdown.js
+++ simple/copy-in-markdown.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copy in Markdown
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Generate current page useful information in Markdown format into clip board.
 // @author       calfzhou
 // @downloadURL  https://cdn.jsdelivr.net/gh/calfzhou/tampermonkey-scripts/simple/copy-in-markdown.js
@@ -33,9 +33,29 @@
     GM_setClipboard(markdown, 'text')
   }
 
+  function copyGithubInfoForHexo(includeRelease = false) {
+    const parts = location.pathname.split('/', 4)
+    const user = parts[1]
+    const repo = parts[2]
+    let markdown =
+      `[<img alt="${user}/${repo}" src="https://img.shields.io/static/v1?label=${user}&message=${repo}&color=blue&logo=github" style="display: inline-block">]` +
+      `(https://github.com/${user}/${repo})` +
+      ` <img alt="stars" src="https://img.shields.io/github/stars/${user}/${repo}?logo=.&style=social" style="display: inline-block">` +
+      ` <img alt="forks" src="https://img.shields.io/github/forks/${user}/${repo}?logo=.&style=social" style="display: inline-block">` +
+      ` <img alt="updated" src="https://img.shields.io/github/last-commit/${user}/${repo}?label=" style="display: inline-block">`
+    if (includeRelease) {
+      markdown +=
+        ` <img alt="release" src="https://img.shields.io/github/v/release/${user}/${repo}?label=" style="display: inline-block">` +
+        ` <img alt="release date" src="https://img.shields.io/github/release-date/${user}/${repo}?label=" style="display: inline-block">`
+    }
+    GM_setClipboard(markdown, 'text')
+  }
+
   const host = location.host
   if (host === 'github.com') {
     GM_registerMenuCommand('Copy repo info badges as Markdown', () => copyGithubInfo())
     GM_registerMenuCommand('Copy repo info & release badges as Markdown', () => copyGithubInfo(true))
+    GM_registerMenuCommand('Copy repo info badges as Hexo Markdown', () => copyGithubInfoForHexo())
+    GM_registerMenuCommand('Copy repo info & release badges as Hexo Markdown', () => copyGithubInfoForHexo(true))
   }
 })()
--- simple/copy-in-markdown.md
+++ simple/copy-in-markdown.md
@@ -2,6 +2,10 @@
 
 Generate current page useful information in Markdown format into clip board.
 
+## 1.0.1 on 2024-04-20
+
+- Add GitHub repo badges for Hexo markdown (need manually set images inline).
+
 ## 1.0.0
 
 - GitHub repository information (with or without release info) using Shields IO badges.
